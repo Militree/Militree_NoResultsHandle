fc5b705fbcf6d52ed938fe2c7cf9888bef6f1664
Brandyn Bold <brandynbold@gmail.com>
2016-07-08 13:28:07 -0400

    Initial commit

---
+++ app/code/community/Militree/NoResultsHandle/Model/Observer.php
@@ -0,0 +1,8 @@
+<?php
+  class Militree_NoResultsHandle_Model_Observer
+  {
+      public function controllerActionLayoutLoadBefore(Varien_Event_Observer $observer)
+      {
+        // Silence is golden
+      }
+  }
